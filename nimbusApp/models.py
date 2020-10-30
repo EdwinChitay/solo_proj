742738cc394376ecdaee84016b0e2a8c419e6f03
Edwin <edwinchitay@yahoo.com>
2020-10-30 00:13:32 -0700

    added models, and reg/login page

--- nimbusApp/models.py
+++ nimbusApp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import re 
 
 # Create your models here.
+class UserManager(models.Manager):
+    def create_validator(self, reqPOST):
+        errors = {}
+        email_checker = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if len(reqPOST['first_name']) <2 or len(reqPOST['last_name']) <2:
+            errors['name'] = "Name must be at least 2 characters"
+        if not email_checker.match(reqPOST['email']):
+            errors['email'] = "Email must be valid"
+        if len(reqPOST['password']) < 8:
+            errors['password'] = "Password must be at least 8 chcaracters"
+        if reqPOST['password'] != reqPOST['confpassword']:
+            errors['password'] = "Passwords do not match!"
+        return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
--- nimbusApp/views.py
+++ nimbusApp/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render, redirect
-
+from .models import *
+from django.contrib import messages
+import bcrypt
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    if request.method == "POST":
+        errors = User.objects.create_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/') 
+        hashed_pw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+        print(hashed_pw)
+        new_user = User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'],
+        email=request.POST['email'], password=hashed_pw)
+        request.session['user_id'] = new_user.id
+        return redirect('/groups')
+    return redirect('/') 
+
+def login(request):
+    logged_user = User.objects.filter(email=request.POST['email'])
+    if len(logged_user) > 0:
+        logged_user = logged_user[0]
+        if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+            request.session['user_id'] = logged_user.id
+            return redirect('/groups')
+    messages.error(request, "Email and/or password are incorrect")
+    return redirect('/')
